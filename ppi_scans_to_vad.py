9f0c24a9d16c2b5ad79861067726a6d7e9e35fd8
Isabel Suhr <isabels@ucar.edu>
2021-07-21 16:12:09 +0000

    Fix two bugs: - if no scans w/ good elevation and azimuth are present, return. - make sure to get ranges, location, etc. from a good PPI scan instead of the most recent one.

--- ppi_scans_to_vad.py
+++ ppi_scans_to_vad.py
@@ -41,20 +41,46 @@ def process(ppi_files, max_cnr, final_path):
     etime = []
     vr_all = []
     mean_cnr = []
+    #need these values from any good PPI scan
+    alt = None
+    lat = None
+    lon = None
+    ranges = None
+    elevation = None
+    azimuth = None
+
     for f in ppi_files:
         ppi = PPI.fromFile(f)
+        
         # for low elevation angles, VAD output isn't very helpful
         # NEED THIS IF STATEMENT IF THE LIST OF PPIs MIGHT USE A DIFFERENT # OF AZIMUTH ANGLES
         # For this case, SWEX ppis need to have 360 azimuth angles
         if ppi.elevation < 6 or len(ppi.azimuth) != 360:
             continue
-
+            
         ppi.threshold_cnr(max_cnr)
-
+        
         vr_all.append(ppi.vr)
         mean_cnr.append(np.nanmean(ppi.cnr, axis=0))
         stime.append(dt.datetime.strptime(ppi.str_start, '%Y-%m-%d %H:%M:%S.%f').timestamp())
         etime.append(dt.datetime.strptime(ppi.str_end, '%Y-%m-%d %H:%M:%S.%f').timestamp())
+        # good scan, so fill in metadata
+        if alt is None:
+            alt = ppi.alt
+        if lat is None:
+            lat = ppi.lat
+        if lon is None:
+            lon = ppi.lon
+        if ranges is None:
+            ranges = ppi.ranges
+        if elevation is None:
+            elevation = ppi.elevation
+        if azimuth is None:
+            azimuth = ppi.azimuth
+
+    if not vr_all: 
+        # vr_all is empty if no files had ok elevation and azimuth. can't continue processing.
+        return
 
     if len(ppi_files) > 1:
         filename_time = dt.datetime.fromtimestamp(stime[0]).strftime('%Y%m%d')
@@ -63,8 +89,8 @@ def process(ppi_files, max_cnr, final_path):
     final_file_name = 'VAD_' + filename_time + '.nc'
     final_file_path = os.path.join(final_path, final_file_name)
 
-    VAD = Lidar_functions.ARM_VAD(vr_all, ppi.ranges, ppi.elevation, ppi.azimuth)
-    VAD.create_ARM_nc(mean_cnr, max_cnr, ppi.alt, ppi.lat, ppi.lon, stime, etime, final_file_path)
+    VAD = Lidar_functions.ARM_VAD(vr_all, ranges, elevation, azimuth)
+    VAD.create_ARM_nc(mean_cnr, max_cnr, alt, lat, lon, stime, etime, final_file_path)
 
 def main():
     args = createParser()
