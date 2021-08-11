71168847dabf77c9367ae04c87b49a2162dbd829
Isabel Suhr <isabels@ucar.edu>
2021-08-11 19:41:04 +0000

    Update tests to match refactoring

--- test_vad.py
+++ test_vad.py
@@ -16,31 +16,31 @@ def ppi():
 
 @pytest.fixture
 def locations():
-    x = pickle.load(open("pickles/x.p", "rb"))
-    y = pickle.load(open("pickles/y.p", "rb"))
-    z = pickle.load(open("pickles/z.p", "rb"))
+    x = pickle.load(open("pickles/vad/x.p", "rb"))
+    y = pickle.load(open("pickles/vad/y.p", "rb"))
+    z = pickle.load(open("pickles/vad/z.p", "rb"))
     return(x,y,z)
 
 @pytest.fixture
 def final_vad_winds():
-    u = pickle.load(open("pickles/u.p", "rb"))
-    v = pickle.load(open("pickles/v.p", "rb"))
-    w = pickle.load(open("pickles/w.p", "rb"))
+    u = pickle.load(open("pickles/vad/u.p", "rb"))
+    v = pickle.load(open("pickles/vad/v.p", "rb"))
+    w = pickle.load(open("pickles/vad/w.p", "rb"))
     return (u, v, w)
 
 @pytest.fixture
 def final_vad_errs():
-    du = pickle.load(open("pickles/du.p", "rb"))
-    dv = pickle.load(open("pickles/dv.p", "rb"))
-    dw = pickle.load(open("pickles/dw.p", "rb"))
+    du = pickle.load(open("pickles/vad/du.p", "rb"))
+    dv = pickle.load(open("pickles/vad/dv.p", "rb"))
+    dw = pickle.load(open("pickles/vad/dw.p", "rb"))
     return (du, dv, dw)
     
 @pytest.fixture
 def derived_products():
-    speed = pickle.load(open("pickles/speed.p", "rb"))
-    wdir = pickle.load(open("pickles/wdir.p", "rb"))
-    res = pickle.load(open("pickles/residual.p", "rb"))
-    cor = pickle.load(open("pickles/correlation.p", "rb"))
+    speed = pickle.load(open("pickles/vad/speed.p", "rb"))
+    wdir = pickle.load(open("pickles/vad/wdir.p", "rb"))
+    res = pickle.load(open("pickles/vad/residual.p", "rb"))
+    cor = pickle.load(open("pickles/vad/correlation.p", "rb"))
     return (speed, wdir, res, cor)
 
 def test_xyz(ppi, locations):
@@ -52,48 +52,48 @@ def test_xyz(ppi, locations):
 
 def test_non_nan_idxs(ppi):
     idxs = non_nan_idxs(ppi.vr, 0)
-    saved_idxs = pickle.load(open("pickles/foo0.p", "rb"))
+    saved_idxs = pickle.load(open("pickles/vad/foo0.p", "rb"))
     assert ma.allequal(idxs, saved_idxs)
     idxs = non_nan_idxs(ppi.vr, 1)
-    saved_idxs = pickle.load(open("pickles/foo1.p", "rb"))
+    saved_idxs = pickle.load(open("pickles/vad/foo1.p", "rb"))
     assert ma.allequal(idxs, saved_idxs)
     idxs = non_nan_idxs(ppi.vr, 2)
-    saved_idxs = pickle.load(open("pickles/foo2.p", "rb"))
+    saved_idxs = pickle.load(open("pickles/vad/foo2.p", "rb"))
     assert ma.allequal(idxs, saved_idxs)
     idxs = non_nan_idxs(ppi.vr, 3)
-    saved_idxs = pickle.load(open("pickles/foo3.p", "rb"))
+    saved_idxs = pickle.load(open("pickles/vad/foo3.p", "rb"))
     assert ma.allequal(idxs, saved_idxs)
 
 def test_calc_A(ppi):
     A = calc_A(ppi.elevation, ppi.azimuth, non_nan_idxs(ppi.vr, 0))
-    saved_A = pickle.load(open("pickles/A_0.p", "rb"))
+    saved_A = pickle.load(open("pickles/vad/A_0.p", "rb"))
     assert ma.allequal(A, saved_A)
     A = calc_A(ppi.elevation, ppi.azimuth, non_nan_idxs(ppi.vr, 1))
-    saved_A = pickle.load(open("pickles/A_1.p", "rb"))
+    saved_A = pickle.load(open("pickles/vad/A_1.p", "rb"))
     assert ma.allequal(A, saved_A)
     A = calc_A(ppi.elevation, ppi.azimuth, non_nan_idxs(ppi.vr, 2))
-    saved_A = pickle.load(open("pickles/A_2.p", "rb"))
+    saved_A = pickle.load(open("pickles/vad/A_2.p", "rb"))
     assert ma.allequal(A, saved_A)
     A = calc_A(ppi.elevation, ppi.azimuth, non_nan_idxs(ppi.vr, 3))
-    saved_A = pickle.load(open("pickles/A_3.p", "rb"))
+    saved_A = pickle.load(open("pickles/vad/A_3.p", "rb"))
     assert ma.allequal(A, saved_A)
 
 def test_calc_b(ppi):
     b = calc_b(ppi.elevation, ppi.azimuth, ppi.vr, non_nan_idxs(ppi.vr, 0), 0)
-    saved_b = pickle.load(open("pickles/b_0.p", "rb"))
+    saved_b = pickle.load(open("pickles/vad/b_0.p", "rb"))
     assert ma.allequal(b, saved_b)
     b = calc_b(ppi.elevation, ppi.azimuth, ppi.vr, non_nan_idxs(ppi.vr, 1), 1)
-    saved_b = pickle.load(open("pickles/b_1.p", "rb"))
+    saved_b = pickle.load(open("pickles/vad/b_1.p", "rb"))
     assert ma.allequal(b, saved_b)
     b = calc_b(ppi.elevation, ppi.azimuth, ppi.vr, non_nan_idxs(ppi.vr, 2), 2)
-    saved_b = pickle.load(open("pickles/b_2.p", "rb"))
+    saved_b = pickle.load(open("pickles/vad/b_2.p", "rb"))
     assert ma.allequal(b, saved_b)
     b = calc_b(ppi.elevation, ppi.azimuth, ppi.vr, non_nan_idxs(ppi.vr, 3), 3)
-    saved_b = pickle.load(open("pickles/b_3.p", "rb"))
+    saved_b = pickle.load(open("pickles/vad/b_3.p", "rb"))
     assert ma.allequal(b, saved_b)
 
 def test_arm_vad(ppi, final_vad_winds, final_vad_errs, derived_products):
-    vad = VAD.calculate_ARM_VAD(ppi.vr, ppi.ranges, ppi.elevation, ppi.azimuth)
+    vad = VAD.calculate_ARM_VAD(ppi)
     saved_u, saved_v, saved_w = final_vad_winds
     assert np.array_equal(vad.u, saved_u, equal_nan=True)
     assert np.array_equal(vad.v, saved_v, equal_nan=True)
@@ -108,3 +108,5 @@ def test_arm_vad(ppi, final_vad_winds, final_vad_errs, derived_products):
     assert np.array_equal(vad.residual, saved_res, equal_nan=True)
     assert np.array_equal(vad.correlation, saved_cor, equal_nan=True)
 
+def test_vadset():
+    pass
