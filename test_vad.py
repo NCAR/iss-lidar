d639c3c350461d9c8456be319c263a749547ddf2
Gary Granger <granger@ucar.edu>
2021-09-13 23:09:43 -0600

    replace allequal with assert_allclose
    and increase the default tolerances enough for all the tests to pass again

--- test_vad.py
+++ test_vad.py
@@ -2,6 +2,7 @@ import pytest
 import pickle
 import numpy.ma as ma
 import numpy as np
+import numpy.testing
 from pathlib import Path
 
 from ppi import PPI
@@ -9,6 +10,14 @@ from vad import xyz, non_nan_idxs, calc_A, calc_b
 from vad import VAD, VADSet
 
 datadir = Path(__file__).parent.joinpath("testdata")
+
+
+def assert_allclose(actual, desired, rtol=1e-06, atol=1e-05,
+                    equal_nan=True, err_msg='', verbose=True) -> None:
+    "Wrap numpy.testing.assert_allclose() with different default tolerances."
+    numpy.testing.assert_allclose(actual, desired, rtol, atol,
+                                  equal_nan, err_msg, verbose)
+
 
 @pytest.fixture
 def ppi():
@@ -61,55 +70,67 @@ def derived_products():
 def test_xyz(ppi, locations):
     saved_x, saved_y, saved_z = locations
     x, y, z = xyz(ppi.ranges, ppi.elevation, ppi.azimuth)
-    assert ma.allequal(x, saved_x)
-    assert ma.allequal(y, saved_y)
-    assert ma.allequal(z, saved_z)
+    assert_allclose(x, saved_x)
+    assert_allclose(y, saved_y)
+    assert_allclose(z, saved_z)
 
 def test_non_nan_idxs(ppi):
     idxs = non_nan_idxs(ppi.vr, 0)
     saved_idxs = pickle.load(open(f"{datadir}/vad/foo0.p", "rb"))
+    assert_allclose(idxs, saved_idxs)
     idxs = non_nan_idxs(ppi.vr, 1)
     saved_idxs = pickle.load(open(f"{datadir}/vad/foo1.p", "rb"))
+    assert_allclose(idxs, saved_idxs)
     idxs = non_nan_idxs(ppi.vr, 2)
     saved_idxs = pickle.load(open(f"{datadir}/vad/foo2.p", "rb"))
+    assert_allclose(idxs, saved_idxs)
     idxs = non_nan_idxs(ppi.vr, 3)
     saved_idxs = pickle.load(open(f"{datadir}/vad/foo3.p", "rb"))
+    assert_allclose(idxs, saved_idxs)
 
 def test_calc_A(ppi):
     A = calc_A(ppi.elevation, ppi.azimuth, non_nan_idxs(ppi.vr, 0))
     saved_A = pickle.load(open(f"{datadir}/vad/A_0.p", "rb"))
+    assert_allclose(A, saved_A)
     A = calc_A(ppi.elevation, ppi.azimuth, non_nan_idxs(ppi.vr, 1))
     saved_A = pickle.load(open(f"{datadir}/vad/A_1.p", "rb"))
+    assert_allclose(A, saved_A)
     A = calc_A(ppi.elevation, ppi.azimuth, non_nan_idxs(ppi.vr, 2))
     saved_A = pickle.load(open(f"{datadir}/vad/A_2.p", "rb"))
+    assert_allclose(A, saved_A)
     A = calc_A(ppi.elevation, ppi.azimuth, non_nan_idxs(ppi.vr, 3))
     saved_A = pickle.load(open(f"{datadir}/vad/A_3.p", "rb"))
+    assert_allclose(A, saved_A)
 
 def test_calc_b(ppi):
     b = calc_b(ppi.elevation, ppi.azimuth, ppi.vr, non_nan_idxs(ppi.vr, 0), 0)
     saved_b = pickle.load(open(f"{datadir}/vad/b_0.p", "rb"))
+    assert_allclose(b, saved_b)
     b = calc_b(ppi.elevation, ppi.azimuth, ppi.vr, non_nan_idxs(ppi.vr, 1), 1)
     saved_b = pickle.load(open(f"{datadir}/vad/b_1.p", "rb"))
+    assert_allclose(b, saved_b)
     b = calc_b(ppi.elevation, ppi.azimuth, ppi.vr, non_nan_idxs(ppi.vr, 2), 2)
     saved_b = pickle.load(open(f"{datadir}/vad/b_2.p", "rb"))
+    assert_allclose(b, saved_b)
     b = calc_b(ppi.elevation, ppi.azimuth, ppi.vr, non_nan_idxs(ppi.vr, 3), 3)
     saved_b = pickle.load(open(f"{datadir}/vad/b_3.p", "rb"))
+    assert_allclose(b, saved_b)
 
 def test_arm_vad(ppi, final_vad_winds, final_vad_errs, derived_products):
     vad = VAD.calculate_ARM_VAD(ppi)
     saved_u, saved_v, saved_w = final_vad_winds
-    assert np.array_equal(vad.u, saved_u, equal_nan=True)
-    assert np.array_equal(vad.v, saved_v, equal_nan=True)
-    assert np.array_equal(vad.w, saved_w, equal_nan=True)
+    assert_allclose(vad.u, saved_u, equal_nan=True)
+    assert_allclose(vad.v, saved_v, equal_nan=True)
+    assert_allclose(vad.w, saved_w, equal_nan=True)
     saved_du, saved_dv, saved_dw = final_vad_errs
-    assert np.array_equal(vad.du, saved_du, equal_nan=True)
-    assert np.array_equal(vad.dv, saved_dv, equal_nan=True)
-    assert np.array_equal(vad.dw, saved_dw, equal_nan=True)
+    assert_allclose(vad.du, saved_du, equal_nan=True)
+    assert_allclose(vad.dv, saved_dv, equal_nan=True)
+    assert_allclose(vad.dw, saved_dw, equal_nan=True)
     saved_wspd, saved_wdir, saved_res, saved_cor = derived_products
-    assert np.array_equal(vad.speed, saved_wspd, equal_nan=True)
-    assert np.array_equal(vad.wdir, saved_wdir, equal_nan=True)
-    assert np.array_equal(vad.residual, saved_res, equal_nan=True)
-    assert np.array_equal(vad.correlation, saved_cor, equal_nan=True)
+    assert_allclose(vad.speed, saved_wspd, equal_nan=True)
+    assert_allclose(vad.wdir, saved_wdir, equal_nan=True)
+    assert_allclose(vad.residual, saved_res, equal_nan=True)
+    assert_allclose(vad.correlation, saved_cor, equal_nan=True)
 
 def test_vadset_netcdf(ppis):
     """ Compare data and data types between VADSet from VADs and VADSet from netcdf """
@@ -122,16 +143,16 @@ def test_vadset_netcdf(ppis):
     # vadset from file should match original vadset
     assert vs.min_cnr == f.min_cnr
     assert type(vs.min_cnr) == type(f.min_cnr)
-    assert np.array_equal(vs.alt, f.alt, equal_nan=True)
+    assert_allclose(vs.alt, f.alt, equal_nan=True)
     assert type(vs.alt) == type(f.alt)
     # differences of like 6e-7 in these vals
     assert vs.lat == pytest.approx(f.lat)
     assert type(vs.lat) == type(f.lat)
     assert vs.lon == pytest.approx(f.lon)
     assert type(vs.lon) == type(f.lon)
-    assert np.array_equal(vs.height, f.height)
+    assert_allclose(vs.height, f.height)
     assert type(vs.height) == type(f.height)
-    assert np.allclose(vs.mean_cnr, f.mean_cnr)
+    assert_allclose(vs.mean_cnr, f.mean_cnr)
     assert type(vs.mean_cnr) == type(f.mean_cnr)
     assert vs.stime == f.stime
     assert type(vs.stime) == type(f.stime)
@@ -141,25 +162,25 @@ def test_vadset_netcdf(ppis):
     assert type(vs.el) == type(f.el)
     assert vs.nbeams == f.nbeams
     assert type(vs.nbeams) == type(f.nbeams)
-    assert np.allclose(vs.u, f.u, equal_nan=True)
+    assert_allclose(vs.u, f.u, equal_nan=True)
     assert type(vs.u) == type(f.u)
-    assert np.allclose(vs.du, f.du, equal_nan=True)
+    assert_allclose(vs.du, f.du, equal_nan=True)
     assert type(vs.du) == type(f.du)
-    assert np.allclose(vs.w, f.w, equal_nan=True)
+    assert_allclose(vs.w, f.w, equal_nan=True)
     assert type(vs.w) == type(f.w)
-    assert np.allclose(vs.dw, f.dw, equal_nan=True)
+    assert_allclose(vs.dw, f.dw, equal_nan=True)
     assert type(vs.dw) == type(f.dw)
-    assert np.allclose(vs.v, f.v, equal_nan=True)
+    assert_allclose(vs.v, f.v, equal_nan=True)
     assert type(vs.v) == type(f.v)
-    assert np.allclose(vs.dv, f.dv, equal_nan=True)
+    assert_allclose(vs.dv, f.dv, equal_nan=True)
     assert type(vs.dv) == type(f.dv)
-    assert np.allclose(vs.speed, f.speed, equal_nan=True)
+    assert_allclose(vs.speed, f.speed, equal_nan=True)
     assert type(vs.speed) == type(f.speed)
-    assert np.allclose(vs.wdir, f.wdir, equal_nan=True)
+    assert_allclose(vs.wdir, f.wdir, equal_nan=True)
     assert type(vs.wdir) == type(f.wdir)
-    assert np.allclose(vs.residual, f.residual, equal_nan=True)
+    assert_allclose(vs.residual, f.residual, equal_nan=True)
     assert type(vs.residual) == type(f.residual)
-    assert np.allclose(vs.correlation, f.correlation, equal_nan=True)
+    assert_allclose(vs.correlation, f.correlation, equal_nan=True)
     assert type(vs.correlation) == type(f.correlation)
 
     
