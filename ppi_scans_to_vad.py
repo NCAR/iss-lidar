66c861c24cb87b8beb4acb84c9a6a4b9808a7e25
Isabel Suhr <isabels@ucar.edu>
2022-10-04 15:19:07 -0600

    Allow list of files as argument to ppi_scans_to_vad, other misc formatting cleanup

--- ppi_scans_to_vad.py
+++ ppi_scans_to_vad.py
@@ -9,11 +9,9 @@ Output VAD winds into ARM netCDF format from cfradial format
 Program works for either 1 cfradial file or multiple within 1 day
 """
 import os
-import sys
 import warnings
 import glob
 import argparse
-import datetime as dt
 import numpy as np
 from vad import VAD, VADSet
 from ppi import PPI
@@ -21,42 +19,45 @@ from ppi import PPI
 warnings.simplefilter("ignore")
 np.set_printoptions(threshold=np.inf)
 
+
 def createParser():
-    parser = argparse.ArgumentParser(description="Generate netCDF of VAD winds from PPI scans")
-    parser.add_argument("--min_cnr", default=-22, type=float, help="threshold cnr below this value")
-    parser.add_argument("ppifiles", help="ppi file(s) for input")
+    parser = argparse.ArgumentParser(description="Generate netCDF of VAD winds"
+                                     " from PPI scans")
+    parser.add_argument("--min_cnr", default=-22, type=float, help="threshold"
+                        " cnr below this value")
     parser.add_argument("destdir", help="directory to save VAD files to")
+    parser.add_argument("ppifiles", help="ppi file(s) for input", nargs='+')
     return parser.parse_args()
 
+
 def selectFiles(path):
-    # get list of all the ppi files for a given day
+    # expand glob expression if it didn't get expanded in the shell
     ppi_files = glob.glob(path)
     return sorted(list(ppi_files))
+
 
 def process(ppi_files, min_cnr):
     vads = []
 
     for f in ppi_files:
         ppi = PPI.fromFile(f)
-        
+
         # for low elevation angles, VAD output isn't very helpful
-        # NEED THIS IF STATEMENT IF THE LIST OF PPIs MIGHT USE A DIFFERENT # OF AZIMUTH ANGLES
-        # For this case, SWEX ppis need to have 360 azimuth angles
         if ppi.elevation < 6:
             continue
         ppi.threshold_cnr(min_cnr)
-        
+
         # generate VAD for this timestep
         vad = VAD.calculate_ARM_VAD(ppi)
         vads.append(vad)
 
-    if not vads: 
+    if not vads:
         # didn't successfully create any vads. can't continue processing.
         return
 
-    
     vadset = VADSet.from_VADs(vads, min_cnr)
     return vadset
+
 
 def save(vadset, destdir, prefix=None):
     filename_time = vadset.stime[0].strftime('%Y%m%d')
@@ -67,11 +68,18 @@ def save(vadset, destdir, prefix=None):
     final_file_path = os.path.join(destdir, final_file_name)
     vadset.to_ARM_netcdf(final_file_path)
 
+
 def main():
     args = createParser()
-    ppi_scans = selectFiles(args.ppifiles)
+    print(type(args.ppifiles))
+    # try to allow either list of files created by shell glob or expression to
+    # pass to python glob
+    ppi_scans = args.ppifiles
+    if len(args.ppifiles) == 1:
+        ppi_scans = selectFiles(args.ppifiles[0])
     vadset = process(ppi_scans, args.min_cnr)
     save(vadset, args.destdir)
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     main()
