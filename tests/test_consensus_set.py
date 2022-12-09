926c6d8fdeb221ce15e679801e619e5a72ec7f3f
Isabel Suhr <isabels@ucar.edu>
2022-12-09 11:10:53 -0700

    Add test for reading consensus netcdf

--- tests/test_consensus_set.py
+++ tests/test_consensus_set.py
@@ -1,11 +1,54 @@
 import datetime as dt
 import pytz
+import numpy as np
 import numpy.ma as ma
 from pathlib import Path
 from consensus_set import ConsensusSet
 from vad import VADSet
 
 datadir = Path(__file__).parent.parent.joinpath("testdata")
+
+
+def compare_consensus(a: ConsensusSet, b: ConsensusSet):
+    """ Compare data from two consensus objects, allowing for small differences
+    from saving/reading files, etc"""
+    np.testing.assert_allclose(a.alt, b.alt)
+    assert isinstance(b.alt, ma.MaskedArray)
+    # differences of like 6e-7 in these vals
+    np.testing.assert_allclose(a.lat, b.lat)
+    assert isinstance(b.lat, ma.MaskedArray)
+    np.testing.assert_allclose(a.lon, b.lon)
+    assert isinstance(b.lon, ma.MaskedArray)
+    np.testing.assert_allclose(a.height, b.height)
+    assert isinstance(b.height, ma.MaskedArray)
+    np.testing.assert_allclose(a.mean_cnr, b.mean_cnr)
+    assert isinstance(b.mean_cnr, ma.MaskedArray)
+    assert a.stime == b.stime
+    assert isinstance(b.stime, list)
+    assert a.etime == b.etime
+    assert isinstance(b.etime, list)
+    np.testing.assert_allclose(a.u, b.u, equal_nan=True)
+    assert isinstance(b.u, ma.MaskedArray)
+    np.testing.assert_allclose(a.n_u, b.n_u, equal_nan=True)
+    assert isinstance(b.n_u, ma.MaskedArray)
+    np.testing.assert_allclose(a.w, b.w, equal_nan=True)
+    assert isinstance(b.w, ma.MaskedArray)
+    np.testing.assert_allclose(a.n_w, b.n_w, equal_nan=True)
+    assert isinstance(b.n_w, ma.MaskedArray)
+    np.testing.assert_allclose(a.v, b.v, equal_nan=True)
+    assert isinstance(b.v, ma.MaskedArray)
+    np.testing.assert_allclose(a.n_v, b.n_v, equal_nan=True)
+    assert isinstance(b.n_v, ma.MaskedArray)
+    np.testing.assert_allclose(a.speed, b.speed, equal_nan=True)
+    assert isinstance(b.speed, ma.MaskedArray)
+    np.testing.assert_allclose(a.wdir, b.wdir, equal_nan=True)
+    assert isinstance(b.wdir, ma.MaskedArray)
+    np.testing.assert_allclose(a.residual, b.residual, equal_nan=True)
+    assert isinstance(b.residual, ma.MaskedArray)
+    np.testing.assert_allclose(a.correlation, b.correlation, equal_nan=True)
+    assert isinstance(b.correlation, ma.MaskedArray)
+    assert a.span == b.span
+    assert a.window == b.window
 
 
 def test_create_time_ranges():
@@ -23,8 +66,9 @@ def test_median_from_consensus_idxs():
     assert median == ma.median([1, 2, 3, 2, 1])
 
 
-def test_write_netcdf():
+def test_write_read_netcdf():
     vs = VADSet.from_file(f"{datadir}/VAD_20220501.nc")
     cs = ConsensusSet.from_VADSet(vs, 5, dt.timedelta(minutes=30))
     cs.to_ARM_netcdf(f"{datadir}/test_consensus.nc")
-
+    cs_file = ConsensusSet.from_file(f"{datadir}/test_consensus.nc")
+    compare_consensus(cs, cs_file)
